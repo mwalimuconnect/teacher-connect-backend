8ced7fea252a8b6bcee16f9ef961ccf2d58c0b99
mwalimuconnect <samuel.oino@yahoo.com>
2026-09-17 15:30:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const connectDB = async () => {
   if (isConnected) return;
   
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
+        const mongoUri = process.env.MONGO_URI || "mongodb+srv://samueloino_db_user:YOUR_PASSWORD@cluster0.xxxxx.mongodb.net/teacherconnect?retryWrites=true&w=majority";
+
+    const db = await mongoose.connect(mongoUri, {
       bufferCommands: false, // Prevents 10000ms buffering timeouts
       serverSelectionTimeoutMS: 5000,
     });
